e41f25cec6b73c01b77a29a39fbe1203a46cda66
mkpatterson <mkp@dataspoke.tech>
2019-04-29 10:12:38 -0400

    adding automated exection logic

--- att&ck_posture/art/execution-frameworks/contrib/python/checkart.py
+++ att&ck_posture/art/execution-frameworks/contrib/python/checkart.py
@@ -1,7 +1,18 @@
-#runner.py
+# Imports runner.py, sets variables and executes scripts
 
 import runner
 
 def main():
-    t = runner.AtomicRunner()
+
+    # Instantiate the AtomicRunner class instance.
+    techniques = runner.AtomicRunner()
+
+    techniques.execute("T1033", position=0, parameters={"computer_name": "localhost"})
+    techniques.execute("T1089", position=0, parameters={"computer_name": "localhost"})
+    techniques.execute("T1086", position=0, parameters={"computer_name": "localhost"})
+    techniques.execute("T1030", position=0,)
+    techniques.execute("T1046", position=0,)
+
+if __name__ == "__main__":
+    main()
 
